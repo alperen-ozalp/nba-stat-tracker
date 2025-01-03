fc4d8bd7245b1d055de22f0e14928bbbccf25d89
Alperen Özalp <ozalpa21@itu.edu.tr>
2025-01-04 00:38:07 +0300

    Table

---
+++ SQL/player_info.sql
@@ -0,0 +1,9 @@
+create table player_info(
+	player_id INT,
+    player_name VARCHAR(255),
+    hof bool,
+    num_seasons INT,
+    first_season INT,
+    last_season INT,
+    primary key(player_id)
+);
---
+++ SQL/team_abbrv.sql
@@ -0,0 +1,7 @@
+CREATE TABLE team_abbrv (
+	season INT,
+    team_id INT,
+    team_name varchar(63),
+    abbreviation varchar(4),
+    primary KEY (team_id)
+);
